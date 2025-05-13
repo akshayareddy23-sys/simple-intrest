562c86f449f88fd6c6bacfe9a805ee82c0c00a6b
akshayareddy23-sys <akshaya.yardly@gmail.com>
2025-05-13 19:30:52 +0530

    Create student fail or pass .c

---
+++ student fail or pass .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int marks;
+    printf("enter student's marks:");
+    scanf("%d",&marks);
+    if (marks >= 40)
+{
+    printf("student passed with %d marks.\n",marks);
+} else {
+    printf("studenty failed with%d marks.\n",marks);
+}
+return 0;
+}
