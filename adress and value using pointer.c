cb79bfb1a6df3f9dc228348be55d9562589b3841
akshayareddy23-sys <akshaya.yardly@gmail.com>
2025-05-15 20:30:24 +0530

    adress and value using pointer.c

---
+++ adress and value using pointer.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+int main()
+{
+    int y=9;
+    int*ptr=&y;
+    printf("y is:%d\n",y);
+    printf("adress is:%d\n",ptr);
+    return 0;
+}
+
+out put
+y is:9
+adress is:-1889674028
+
+
+=== Code Execution Successful ===
