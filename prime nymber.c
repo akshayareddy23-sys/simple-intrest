c33f63c219490d37b38a1dea8545dcfac2536e66
akshayareddy23-sys <akshaya.yardly@gmail.com>
2025-05-16 10:54:59 +0530

    prime number.c

---
+++ prime nymber.c
@@ -0,0 +1,30 @@
+# include<stdio.h>
+int i,num,isprime=1; 
+int main()
+{
+    printf("enter the num starting from positive 2");
+    scanf("%d",&num);
+        for(i=2;i<=num/2;i++){
+            if (num%2==0){
+                printf("its is not a prime \n");
+            }
+                else 
+                {
+                    printf("it is a prime \n");
+                    
+                }
+                    
+                }
+            }
+
+out put
+enter the num starting from positive 2
+13
+it is a prime 
+it is a prime 
+it is a prime 
+it is a prime 
+it is a prime 
+
+
+=== Code Execution Successful ===
