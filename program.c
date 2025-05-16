9c7d114abee2e33276b8b7e49d5c87a716ebd7d6
akshayareddy23-sys <akshaya.yardly@gmail.com>
2025-05-16 16:06:41 +0530

    Create program.c

---
+++ program.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    struct akshaya{
+        char name;
+        int age;
+        float weight;
+    };
+    struct akshaya r1;
+printf("enter the name");
+scanf("%s",&r1.name);
+printf("enter the age");
+scanf("%d",&r1.age);
+printf("enter the weight");
+scanf("%d",&r1.weight);
+}
+
