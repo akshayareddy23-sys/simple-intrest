e8bc4742cf6eebf7687e0744da1067a44d5ca41c
akshayareddy23-sys <akshaya.yardly@gmail.com>
2025-05-17 11:03:48 +0530

    area of rectanglr 2 .c

---
+++ area of rectanglr 2 .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float length,width,area;
+    printf("enter the length of the rectangle");
+    scanf("%f",&length);
+    printf("enter the width of the rectanhle");
+    scanf("%f",&width);
+area=length*width;
+printf("area of the rectangle:%.2f\n",area);
+}
